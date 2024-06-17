af50aa24b36408edaf814bf69e52c935259e3ede
Nipun Chamika <nipunchamika11@gmail.com>
2024-06-17 14:02:19 +0530

    feat: Configure Firebase

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import pyrebase
 
-# Create your views here.
+
+config = {
+    "apiKey": "AIzaSyD8LKb-1rSspyI1J1S5gh6226niCs4qLrI",
+    "authDomain": "user-hub-project.firebaseapp.com",
+    "databaseURL": "https://user-hub-project-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "user-hub-project",
+    "storageBucket": "user-hub-project.appspot.com",
+    "messagingSenderId": "1087560919378",
+    "appId": "1:1087560919378:web:eef9b3f627c28ec17bb28b"
+}
+
+firebase = pyrebase.initialize_app(config)
+authe = firebase.auth()
+db = firebase.database()
+
+
+def index(request):
+    return render(request, 'index.html')
